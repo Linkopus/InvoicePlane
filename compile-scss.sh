f1d0e2215e10219ce0b21d24a7da3af27d3226d3
iliaselmakhfi <74756014+iliaselmakhfi@users.noreply.github.com>
2024-07-07 13:10:04 +0200

    add step to compile scss to css

--- compile-scss.sh
+++ compile-scss.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
 
-SRC_DIR="assets/invoiceplane/sass"
-DEST_DIR="assets/invoiceplane/css"
+SRC_INVOICEPLANE_DIR="assets/invoiceplane/sass"
+DEST_INVOICEPLANE_DIR="assets/invoiceplane/css"
 
 mkdir -p $DEST_DIR
 
-for file in $SRC_DIR/*.scss; do
+for file in $SRC_INVOICEPLANE_DIR/*.scss; do
   filename=$(basename "$file" .scss)
-  node-sass "$file" "$DEST_DIR/$filename.css"
+  node-sass "$file" "$DEST_INVOICEPLANE_DIR/$filename.css"
+done
+
+SRC_INVOICEPLANE_BLEU_DIR="assets/invoiceplane_blue/sass"
+DEST_INVOICEPLANE_BLEU_DIR="assets/invoiceplane_blue/css"
+
+mkdir -p $DEST_DIR
+
+for file in $SRC_INVOICEPLANE_BLEU_DIR/*.scss; do
+  filename=$(basename "$file" .scss)
+  node-sass "$file" "$DEST_INVOICEPLANE_BLEU_DIR/$filename.css"
 done
